bc25d71f783cb6351ab61e923f7c97e282323b4c
m.urbaniak <urbaniak.michal@yahoo.com>
2022-07-23 21:37:50 +0200

    add linkedin and email info

---
+++ src/main-page/headerLinkedin/HeaderPersonalInfo.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './HeaderPersonalInfo.scss';
+import {BsLinkedin} from 'react-icons/bs';
+import {MdEmail} from 'react-icons/md';
+
+
+const HeaderPersonalInfo = () => {
+    return (
+        <React.Fragment>
+            <header className='header-top-container'>
+                    <address className='header-email-wrapper'>
+                        <MdEmail className='email-icon'/>
+                        <a href="mailto:urbaniak.michal@yahoo.com">Send Email</a>
+                    </address>
+                    <address className='header-linkedin-wrapper'> 
+                        <BsLinkedin className="linkedin-icon"/>
+                        <a href='https://www.linkedin.com/in/urltomichalurbaniakprofile/' target="_blank">My Linkedin profile :)</a>
+                    </address>
+            </header>
+        </React.Fragment>
+    )
+}
+
+export default HeaderPersonalInfo;
