2151cd88fe7ad14be14abcb5182b86f21bb91779
m.urbaniak <urbaniak.michal@yahoo.com>
2022-07-24 12:53:04 +0200

    add my email and li link

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.scss';
 import HeaderPersonalInfo from './main-page/headerLinkedin/HeaderPersonalInfo';
 
+
 function App() {
   return (
     <BrowserRouter>
--- src/main-page/headerLinkedin/HeaderPersonalInfo.jsx
+++ src/main-page/headerLinkedin/HeaderPersonalInfo.jsx
@@ -8,14 +8,14 @@ const HeaderPersonalInfo = () => {
     return (
         <React.Fragment>
             <header className='header-top-container'>
-                    <address className='header-email-wrapper'>
-                        <MdEmail className='email-icon'/>
-                        <a href="mailto:urbaniak.michal@yahoo.com">Send Email</a>
-                    </address>
-                    <address className='header-linkedin-wrapper'> 
-                        <BsLinkedin className="linkedin-icon"/>
-                        <a href='https://www.linkedin.com/in/urltomichalurbaniakprofile/' target="_blank">My Linkedin profile :)</a>
-                    </address>
+                <address className='header-email-wrapper'>
+                    <MdEmail className='email-icon'/>
+                    <a href="mailto:urbaniak.michal@yahoo.com">Send Email</a>
+                </address>
+                <address className='header-linkedin-wrapper'> 
+                    <BsLinkedin className="linkedin-icon"/>
+                    <a href='https://www.linkedin.com/in/urltomichalurbaniakprofile/' target="_blank">My Linkedin profile :)</a>
+                </address>
             </header>
         </React.Fragment>
     )
