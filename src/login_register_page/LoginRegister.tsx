3ee145b2f7cb5aad0534a562d14f9be08d800aa9
m.urbaniak <urbaniak.michal@yahoo.com>
2022-07-31 09:03:00 +0200

    simple register link

--- src/login_register_page/LoginRegister.tsx
+++ src/login_register_page/LoginRegister.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Login from './login/Login';
 import './LoginRegister.scss';
+import Register from './register/Register';
 
 const LoginRegister = () => {
     return (
         <React.Fragment>
             <section className='login-register-container'>
                 <Login></Login>
+                <Register></Register>
             </section>
         </React.Fragment>
     )
