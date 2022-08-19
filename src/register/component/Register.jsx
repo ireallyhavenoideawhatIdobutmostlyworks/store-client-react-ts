25ee462ed6170b17acd4403e6dfaa9a886da622b
m.urbaniak <urbaniak.michal@yahoo.com>
2022-08-19 19:56:58 +0200

    Fix/remove value tag register component (#18)
    remove value taq from register component

--- src/register/component/Register.jsx
+++ src/register/component/Register.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { RegisterService } from '../service/RegisterService';
 import './Register.scss';
+import UseValidation from '../service/UseValidation'
+import test from '../service/UseValidation'
 
 const Register = () => {
 
@@ -15,60 +17,68 @@ const Register = () => {
         confirmPassword: ""
     });
 
-    const [errors, setErrors] = useState({
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        confirmPassword: ''
-    });
+    // const [errors, setErrors] = useState({
+    //     firstName: "",
+    //     lastName: "",
+    //     email: "",
+    //     password: "",
+    //     confirmPassword: ''
+    // });
+
+    
+    
+    const {handleChange, values, errors, handleSubmit} = UseValidation();
+  
+
 
     const onUpdateField = event => {
+
         setValuesRegisterForm({...valuesRegisterForm, [event.target.name]: event.target.value});
 
-        switch(event.target.name) { 
-            case 'firstName':
-            case 'lastName': { 
-                if(/[^a-zA-Z]/.test(event.target.value)) {
-                    setErrors({...errors, [event.target.name]: 'Only alphabetic characters are allowed'});
-                    setFormValid(false);
-                } else {
-                    setErrors({...errors, [event.target.name]: ''});
-                    setFormValid(true);
-                }
-                break; 
-            } 
-            case 'email': { 
-                if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(event.target.value))) {
-                    setErrors({...errors, [event.target.name]: 'Incorrect email address'});
-                    setFormValid(false);
-                } else {
-                    setErrors({...errors, [event.target.name]: ''});
-                    setFormValid(true);
-                }
-                break; 
-            } 
-            case 'password': { 
-                if(event.target.value.length < 8){
-                    setErrors({...errors, [event.target.name]: 'Password is too short'});
-                    setFormValid(false);
-                } else {
-                    setErrors({...errors, [event.target.name]: ''});
-                    setFormValid(true);
-                }
-                break; 
-            } 
-            case 'confirmPassword': { 
-                if(valuesRegisterForm.password !== event.target.value){
-                    setErrors({...errors, [event.target.name]: 'Confirm password should be same like password'});
-                    setFormValid(false);
-                } else {
-                    setErrors({...errors, [event.target.name]: ''});
-                    setFormValid(true);
-                }
-                break; 
-            } 
-         }
+        // switch(event.target.name) { 
+        //     case 'firstName':
+        //     case 'lastName': { 
+        //         if(/[^a-zA-Z]/.test(event.target.value)) {
+        //             setErrors({...errors, [event.target.name]: 'Only alphabetic characters are allowed'});
+        //             setFormValid(false);
+        //         } else {
+        //             setErrors({...errors, [event.target.name]: ''});
+        //             setFormValid(true);
+        //         }
+        //         break; 
+        //     } 
+        //     case 'email': { 
+        //         if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(event.target.value))) {
+        //             setErrors({...errors, [event.target.name]: 'Incorrect email address'});
+        //             setFormValid(false);
+        //         } else {
+        //             setErrors({...errors, [event.target.name]: ''});
+        //             setFormValid(true);
+        //         }
+        //         break; 
+        //     } 
+        //     case 'password': { 
+        //         if(event.target.value.length < 8){
+        //             setErrors({...errors, [event.target.name]: 'Password is too short'});
+        //             setFormValid(false);
+        //         } else {
+        //             setErrors({...errors, [event.target.name]: ''});
+        //             setFormValid(true);
+        //         }
+        //         break; 
+        //     } 
+        //     case 'confirmPassword': { 
+        //         if(valuesRegisterForm.password !== event.target.value){
+        //             setErrors({...errors, [event.target.name]: 'Confirm password should be same like password'});
+        //             setFormValid(false);
+        //         } else {
+        //             setErrors({...errors, [event.target.name]: ''});
+        //             setFormValid(true);
+        //         }
+        //         break; 
+        //     } 
+        //  }
+
     };
 
     const onSubmitForm = event => {
@@ -79,6 +89,10 @@ const Register = () => {
         } else {
             alert(JSON.stringify('inwalid form', null, 2));
         }
+
+
+
+        
     };
 
     return (
@@ -89,13 +103,12 @@ const Register = () => {
                         <h3>Register now!</h3>
                     </div>
                     <div>
-                        <form className="form-wrapper" onSubmit={onSubmitForm}>
+                        <form className="form-wrapper" onSubmit={handleSubmit}>
                             <div className="form-input-wrapper">
                                 <label>First name</label>
                                 <div>
                                     <input 
-                                        onChange={onUpdateField}
-                                        value={valuesRegisterForm.firstName}
+                                        onChange={handleChange}
                                         type="text" 
                                         placeholder="First name..." 
                                         name="firstName">
@@ -112,8 +125,7 @@ const Register = () => {
                                 <label>Last name</label>
                                 <div>
                                     <input 
-                                        onChange={onUpdateField}
-                                        value={valuesRegisterForm.lastName}
+                                        onChange={handleChange}
                                         type="text" 
                                         placeholder="Last name..." 
                                         name="lastName">
@@ -130,8 +142,7 @@ const Register = () => {
                                 <label>Email address</label> 
                                 <div>
                                     <input 
-                                        onChange={onUpdateField}
-                                        value={valuesRegisterForm.email}
+                                        onChange={handleChange}
                                         type="text" 
                                         placeholder="Email address..." 
                                         name="email">
@@ -148,8 +159,7 @@ const Register = () => {
                                 <label>Password</label>   
                                 <div>
                                     <input 
-                                        onChange={onUpdateField}
-                                        value={valuesRegisterForm.password}
+                                        onChange={handleChange}
                                         type="password" 
                                         placeholder="Password..." 
                                         name="password">
@@ -166,8 +176,12 @@ const Register = () => {
                                 <label>Confirm password</label>   
                                 <div>
                                     <input 
+<<<<<<< HEAD
+                                        onChange={handleChange}
+=======
                                         onChange={onUpdateField}
                                         value={valuesRegisterForm.confirmPassword}
+>>>>>>> bff8123036ea922f215160ea3437c7c2d1108be2
                                         type="password" 
                                         placeholder="Confirm password..." 
                                         name="confirmPassword">
