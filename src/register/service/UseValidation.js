cc03be4ee9ac35083caab45c2856a6b098bf58f5
m.urbaniak <urbaniak.michal@yahoo.com>
2022-08-20 08:53:04 +0200

    resolved conflicts

--- src/register/service/UseValidation.js
+++ src/register/service/UseValidation.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from 'react';
 
 const UseValidation = () => {
@@ -6,35 +5,6 @@ const UseValidation = () => {
     const [isFormValid, setFormValid] = useState(false);
     const [values, setValues] = useState({});
     const [errors, setErrors] = useState({});
-=======
-import React, { useState } from 'react';
-import { TbChevronsDownLeft } from 'react-icons/tb';
-
-// interface RegisterForm {
-//     name: string;
-//     value: string;
-// }
-
-// const test = () => {
-//     console.log('dupa')
-// }
-
-
-
-const UseValidation = (callback) => {
-
-    const [isFormValid, setFormValid] = useState(false);
-    const [values, setValues] = useState({});
-    const [errors, setErrors] = useState({
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        confirmPassword: ''
-    });
-
-
->>>>>>> 25ee462ed6170b17acd4403e6dfaa9a886da622b
 
     const validate = (name, value) => { 
         switch(name) { 
@@ -60,11 +30,7 @@ const UseValidation = (callback) => {
                 break; 
             } 
             case 'password': { 
-<<<<<<< HEAD
                 if(value.length < 8){
-=======
-                if(value.length < 8 ){
->>>>>>> 25ee462ed6170b17acd4403e6dfaa9a886da622b
                     setErrors({...errors, [name]: 'Password is too short'});
                     setFormValid(false);
                 } else {
@@ -73,7 +39,6 @@ const UseValidation = (callback) => {
                 }
                 break; 
             } 
-<<<<<<< HEAD
             case 'confirmPassword': { 
                 if(values.password !== value){
                     setErrors({...errors, [name]: 'Confirm password should be same like password'});
@@ -93,36 +58,6 @@ const UseValidation = (callback) => {
     }
 
     return {onUpdate, values, errors, isFormValid}
-=======
-            // case 'confirmPassword': { 
-            //     if(valuesRegisterFormPassword !== event.target.value){
-            //      //   setErrors({...errors, [event.target.name]: 'Confirm password should be same like password'});
-            //      //   setFormValid(false);
-            //     } else {
-            //        // setErrors({...errors, [event.target.name]: ''});
-            //        // setFormValid(true);
-            //     }
-            //     break; 
-            // } 
-         }
-    }
-
-    const handleChange = (event) => {
-        event.persist();
-
-        validate(event.target.name, event.target.value);
-
-        setValues({...values,[event.target.name]: event.target.value})
-    }
-
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(errors)     
-    }
-
-    return {values, errors, handleChange, handleSubmit}
->>>>>>> 25ee462ed6170b17acd4403e6dfaa9a886da622b
 }
 
 export default UseValidation
