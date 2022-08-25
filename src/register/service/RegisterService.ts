d466e7713b112965c40429b2b5a14f53cdf12c42
m.urbaniak <urbaniak.michal@yahoo.com>
2022-08-25 13:50:03 +0200

    clean up register service file

--- src/register/service/RegisterService.ts
+++ src/register/service/RegisterService.ts
@@ -1,14 +1,6 @@
 export class RegisterService {
 
-    isFieldValid(value: string): boolean {
-        return value !== '' && value !== 'undefined';
-
-    }
-
-    isValidEmail(email: string): boolean {
-        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
-    }
-    
+ 
    
     
   }
